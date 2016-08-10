01241010efd744cfbd850cfa13cbf97b6ba79bbc
Cox <jcox@NATL2236LJCOX.local>
2016-08-10 14:12:37 -0400

    positioning of text captions sort of works

--- script.jsx
+++ script.jsx
@@ -1,72 +1,3 @@
-class ProgressBar {
-
-  constructor(data) {
-    var margin = {top: 20, right: 20, bottom: 50, left: 70},
-    width = 700 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
-
-    var svg = d3.select('.progressBar')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.right + ')');
-
-    var x = d3.scale.linear()
-      .range([0, width])
-      .domain([0, data.length])
-
-    this.circles = svg.selectAll('circle')
-      .data(data)
-        .enter()
-      .append('circle')
-      .attr('cx', function(d, i) { return x(i)})
-      .attr('r', 8)
-      .attr('fill', 'white')
-      .attr('stroke', 'black')
-      .attr('stroke-width', 1)
-      .on('click', function(d,i) {this.fill(i)}.bind(this))
-      .on('mouseover', function(d, i) {
-          d3.select(this).attr('stroke-width', '4px') 
-          focus.transition()
-            .duration(200)
-            .style('display', null)
-          tooltip(d, i)})
-      .on('mouseout', function(d, i) {
-          d3.select(this).attr('stroke-width', '1px') 
-          focus.transition()
-            .duration(200)
-            .style('display', 'none')
-      })
-
-    var focus = svg.append('g')
-      
-    focus.append('text')
-        .attr('class', 'y1')
-        .style('stroke', 'white')
-        .style('stroke-width', '3.5px')
-        .attr('dx', 8)
-        .attr('dy', '-.3em')
-
-    focus.append('text')
-        .attr('class', 'y2')
-        .attr('dx', 8)
-        .attr('dy', '-.3em')
-
-    function tooltip(d, i) {
-      focus.select('text.y1')
-        .attr('transform', 'translate(' + x(i) + ',0)')
-        .text(d.title)
-      focus.select('text.y2')
-        .attr('transform', 'translate(' + x(i) + ',0)')
-        .text(d.title)
-    }
-  }
-
-  fill() {}
-}
-
 class Canvas {
   constructor(data) {
     var margin = {top: 20, right: 20, bottom: 20, left: 20},
@@ -129,19 +60,12 @@ class App extends React.Component {
     this.state = { x: 0, started: false };
     this.handleClick = this.handleClick.bind(this);
     this.start = this.start.bind(this);
-    this.progressBar = new ProgressBar(data);
     this.canvas = new Canvas(data);
     this.faded = {opacity: .4, transition: 'opacity 1s'}
   }
 
   componentDidMount() {
-    this.progressBar.fill = (id) => {
-      this.progressBar.circles.attr('fill', (d, i) => {
-        return ((i <= id) ? 'black' : 'white') 
-      })
-    }
     this.canvas.step = () => {
-      this.progressBar.fill(this.state.x)
       this.canvas.points.attr('display', (d,i) => {
         return (this.state.x == i) ? null : 'none'
       })
@@ -153,7 +77,6 @@ class App extends React.Component {
       })
       this.setState({x: this.state.x += 1})
     }
-    this.progressBar.fill(0)
   }
   
   handleClick() {
