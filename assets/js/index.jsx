8281811074b1fd7780417c2dd9e27c50da59b9c0
Cox <jcox@NATL2236LJCOX.local>
2016-08-24 13:21:55 -0400

    styles and added titles for each step

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -2,24 +2,34 @@ import ProgressBar from './progress-bar';
 import Canvas from './canvas';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '!style!css!sass!../css/vendor/foundation.min.css';
 import '!style!css!sass!../css/style.scss';
+import '../img/ajc-logo.png';
 
 const DATA = [
   {desc: 'This is Stepr. It\'s a responsive single-page app that displays a sequence of events',
+    title: 'First step',
     x: 20, y: 50},
   {desc: 'The user clicks, taps, or uses the arrow keys to step through the events',
+    title: 'Second step is really cool',
     x: 50, y: 40},
   {desc: 'Circles overlaid on a map/aerial photo show where each event took place', 
+    title: 'Wow what a great third step',
     x: 75, y: 20},
   {desc: 'A progress bar at the top of the screen tracks how far the user has left to go',
+    title: 'Fourth step deserves a medal',
     x: 43, y: 80},
   {desc: 'It can be hard to convey spatial logic with words. But with Stepr it\'s easy',
+    title: 'Hi I\'m the fifth step',
     x: 15, y: 40},
   {desc: 'Try it out on mobile - it\'s responsive!',
+    title: 'Sixth step reporting for duty',
     x: 43, y: 80},
   {desc: 'Just make sure that the photo you upload has an aspect ratio of 5:6',
+    title: 'Seven here',
     x: 90, y: 20},
   {desc: 'That\'s it. Try it out in one of your stories!',
+    title: 'Wow eight',
     x: 14, y: 30}
 ]
 
@@ -37,6 +47,27 @@ class App extends React.Component {
     this.hidden = {opacity: 0, transition: 'opacity 1s'};
 
     this.state = {started: false, finished: false, step: -1};
+
+    // Add titles
+    this.data.forEach((s) => {
+      $('#titles').append(`<div class="title">${s.title}</div>`)
+    })
+
+    this.titles = document.getElementsByClassName('title');
+
+    for (var i = 0; i < this.data.length; i++) {
+      ((j) => {
+        this.titles[j].addEventListener('mouseenter', (e) => {
+          e.target.style['margin-left'] = '10px'
+        })
+        this.titles[j].addEventListener('mouseout', (e) => {
+          e.target.style['margin-left'] = '0'
+        })
+        this.titles[j].addEventListener('click', (e) => {
+          this.setStep(j)
+        })
+      })(i)
+    }
   }
 
   componentDidMount() {
@@ -47,27 +78,18 @@ class App extends React.Component {
   }
   
   handleClick(i) {
-    switch(i) {
-      case -1: 
-        if (this.state.step > 0) {
-          this.setState({step: this.state.step += i, finished:false});
-          this.canvas.advance(this.state.step);
-          this.progressBar.fill(this.state.step);
-        }
-        break;
-      case 1:
-        if (this.state.step < this.data.length - 1) {
-          this.setState({step: this.state.step += i});
-          this.canvas.advance(this.state.step);
-          this.progressBar.fill(this.state.step);
-        }
-        else if (this.state.step == this.data.length - 1){
-          this.setState({step: this.state.step += i, finished: true});
-          this.canvas.advance(this.state.step);
-          this.progressBar.fill(this.state.step);
-        }
-        break;
+    this.setStep(this.state.step += i)
+  }
+
+  setStep(step) {
+    this.setState({step: step, finished:false});
+    this.canvas.advance(this.state.step);
+    this.progressBar.fill(this.state.step);
+
+    for (var i=0; i<this.data.length; i++) {
+      this.titles[i].style.color = '#fff';
     }
+    this.titles[this.state.step].style.color = '#49709F';
   }
 
   start() {
