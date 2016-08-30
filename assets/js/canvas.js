a3c76857f9b678f931899f0507b398219219f732
Emily Merwin <emilymerwinajc@gmail.com>
2016-08-30 18:10:39 -0400

    Revert "BROKEN working on adding SVG to page"
    This reverts commit 1fcd82cd4c36996f7b0dd4b994dedda6265a63cf.

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -11,7 +11,7 @@ export default class {
       console.log('resizing canvas')
     })
 
-    this.canvas = d3.select('div#canvas')
+    this.svg = d3.select('div#canvas').append('svg')
         .attr('width', this.width) 
         .attr('height', '100%')
         .attr('position', 'absolute')
