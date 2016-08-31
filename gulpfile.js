8c2d06b1bd2ad6836bc5c31e96ba6b987c8c0f0d
Cox <jcox@NATL2236LJCOX.local>
2016-08-31 15:01:00 -0400

    server works as separate task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,81 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+var gulp = require('gulp'),
+  rename = require('gulp-rename'),
+  buffer = require('vinyl-buffer'),
+  livereload = require('gulp-livereload'),
+  source = require('vinyl-source-stream'),
+  sourcemaps = require('gulp-sourcemaps'),
+  rimraf = require('rimraf');
+  sass = require('gulp-sass'),
+  browserify = require('browserify'),
+  babelify = require('babelify'),
+  webserver = require('gulp-webserver');
+
+var SRC = './assets/';
+var PROD = './dist/prod/';
+var DEV = './dist/dev/';
+
+gulp.task('compile', function() {
+  var b = browserify({
+    entries: SRC + 'js/index.js',
+    debug: true,
+    transform: [babelify.configure({
+      presets: ['es2015']
+    })]
+  })
+  return b.bundle()
+    .pipe(source(SRC + 'js/index.js'))
+    .pipe(buffer())
+    .pipe(rename('main.js'))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(DEV));
+});
 
 gulp.task('sass', function(){
-  return gulp.src('assets/css/style.scss')
-    .pipe(sass({includePaths: ['node_modules/foundation-sites/scss']})) // Using gulp-sass
-    .pipe(gulp.dest('dist/prod/css/'));
+  return gulp.src([SRC + 'css/*.scss', SRC + 'css/**/*.css'])
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest(DEV + 'css/'));
+});
+
+
+gulp.task('build-assets', function(done) {
+  var html = gulp.src(SRC + 'index.html')
+  .pipe(gulp.dest(DEV));
+
+  var fonts = gulp.src(SRC + 'css/fonts/**/*.otf')
+    .pipe(gulp.dest(DEV + 'css/fonts'));
+
+  var images = gulp.src(SRC + 'img/**')
+    .pipe(gulp.dest(DEV + 'img'));
+})
+
+gulp.task('watch-sass', function() {
+  return gulp.watch(SRC + 'css/*.scss', ['sass']);
+});
+
+gulp.task('watch-js', function() {
+  return gulp.watch(SRC + 'js/*.js', ['compile']);
+});
+
+gulp.task('watch-assets', function() {
+  return gulp.watch([SRC + 'index.html', SRC + 'img/*'], ['compile']);
+})
+
+gulp.task('webserver', function() {
+  gulp.src('./dist/dev')
+    .pipe(webserver({
+      port: '8080',
+      livereload: true,
+      open: true
+    }))
+})
+
+gulp.task('watch', [
+  'sass', 
+  'compile', 
+  'build-assets', 
+  'watch-sass', 
+  'watch-js', 
+  'watch-assets'
+  ], function() {
 });
