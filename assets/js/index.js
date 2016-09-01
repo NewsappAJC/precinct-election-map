d60a0ff2dbdb3acbbb5925da44cd790a3bc98afb
Cox <jcox@NATL2236LJCOX.local>
2016-09-01 12:40:49 -0400

    set pin properties with jquery instead of d3. make code more concise and add comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,6 @@ import Data from './data';
 import ProgressBar from './progress-bar';
 import Canvas from './canvas';
 import Stories from './stories';
-import * as d3 from 'd3';
 import $ from 'jquery';
 
 class App {
@@ -15,13 +14,12 @@ class App {
     var Input = new Data;
     this.data = Input.data;
 
-    // Create a new canvas object, which is where the pins 
+    // Select the canvas object where the pins 
     // will be drawn.
-    this.canvas = d3.select('div#canvas');
+    this.canvas = $('#canvas');
 
     // Hide the back button until the user navigates to the next step.
-    this.backButton = document.getElementById('back-button');
-    this.backButton.style.display = 'none';
+    $('#back-button').hide();
 
     // Display a bar across the top of the screen that gradually
     // fills in as the user navigates through the app.
@@ -29,125 +27,105 @@ class App {
     this.progressBar.build(this.data.length + 1)
     this.progressBar.fill(this.step)
 
+    // Render a list of related stories.
     var stories = new Stories();
     stories.render();
 
     // Add an event handler to the splash screen's "Begin" button
     // that hides the splash screen.
-    var beginButton = document.getElementById('begin-button');
-    beginButton.addEventListener('click', () => {
-      var splash = document.getElementById('splash');
-      splash.style.display = 'none';
+    $('#begin-button').click(() => {
+      $('#splash').hide();
     })
 
-    var backButton = document.getElementById('back-button');
-    backButton.addEventListener('click', () => {
+    // Add event handlers to the back and next buttons, and the photo
+    $('#back-button').click(() => {
       this.handleClick(-1);
-    });
-
-    var nextButton = document.getElementById('next-button');
-    nextButton.addEventListener('click', () => {
+    })
+    $('#next-button').click(() => {
       this.handleClick(1);
-    });
+    })
+    $('#wrapper').click(() => {
+      this.handleClick(1)
+    })
 
-    for (var i = 0; i < this.data.length; i++) {
-      // Append divs with descriptions of each step.
-      $('#descs').append(`<div class="desc">
+    // Append divs with descriptions of each step.
+    for (var i in this.data) {
+      $('#descs').append(`
+        <div class="desc">
           <div class="box">
             ${this.data[i].desc}
           </div>
-        </div>`);
+        </div>
+      `);
     }
 
-    // Handle arrow keys as well as clicks.
-    $(document).keydown((e) => {
-      switch(e.which) {
-        case 37: 
-          this.handleClick(-1);
-          break;
+    // Create an instance variable referring to these descriptions.
+    this.descs = $('.desc');
 
-        case 39:
-          this.handleClick(1);
-          break;
-
-        default: return;
-      }
-      e.preventDefault()
-    })
-
-    // Change the opacity of the cover div to display text on top 
-    // of the picture
-    this.cover = document.getElementById('cover');
-
-    // These descriptions will be displayed below or to the left of the picture
-    this.descs = document.getElementsByClassName('desc');
-
-    // Last of all, display the instructions
+    // Last of all, display the instructions.
     this.getMessage();
-
-    var wrapper = document.getElementById('wrapper');
-    wrapper.addEventListener('mouseup', this.handleClick.bind(this, 1))
   }
+
 
   /*
    * Check whether the user is moving forward or backward, and display the 
-   * correct descriptions and pins accordingly.
-   */
+   * correct descriptions and pins accordingly. 
+  */
   handleClick(i) {
     this.setStep(this.step + i)
   }
 
+
   /* 
-   * Change the opacity of different pins depending on which step is
-   * currently displayed, and if necessary display the "finished" message
-   * or the instructions
+   * Hide or change the opacity of pins depending on which step is
+   * currently displayed. If necessary display the "finished" message
+   * or the instructions.
   */
   setStep(step) {
-    this.cover.style.opacity = 0;
+    // Hide the cover div.
+    $('#cover').css({'opacity': 0});
 
-    if (step <= -1) {
-      this.backButton.style.display = 'none';
-      return;
-    };
-
-    // Hide all the descriptions. The description that corresponds to the 
-    // step will be displayed later
-    for (var i=0; i<this.data.length; i++) {
-      this.descs[i].style.display = 'none';
-    };
+    // Hide all the descriptions. The description that corresponds to this
+    // step will be displayed later.
+    this.descs.hide();
 
     // Check if the user has finished the last step in the visualization.
-    // If so, display a finished message
+    // If so, display a finished message, and prevent the step from incrementing.
     if (step >= this.data.length) {
       this.step = this.data.length;
       this.finished = true;
-      this.cover.style.opacity = .7;
-      this.getMessage();
+      $('#cover').css({'opacity': .7});
     }
+
     else if (step < this.data.length){
-      this.backButton.style.display = 'initial';
+      this.step = step;
+
+      // Only display back button if the user is past the first step.
+      var backDisplay = step <= 0 ? 'none' : 'initial';
+      $('#back-button').css({'display': backDisplay});
+
+      // Assign the correct item in the data object to the entry variable.
       var entry = this.data[step];
 
-      this.step = step;
       this.started = true;
       this.finished = false;
 
-      this.descs[this.step].style.display = 'initial';
+      // Unhide the relevant div
+      this.descs[step].style.display = 'initial';
 
       var colors = ['green', 'blue', 'orange']
 
-      this.canvas.append('img')
-        .attr('src', 'img/' + colors[entry.id] + '-pin.svg')
-        // Each pointer's class contains a number that links it
-        // to one of the characters in the visualization.
-        .attr('class', 'point character' + entry.id) 
-        .style('left', entry.x + '%')
-        .style('top', entry.y + '%')
+      // Append a pin for this step.
+      $('#canvas').append(`
+        <img src="./img/${ colors[entry.id] }-pin.svg"
+          class="point character${ entry.id }"
+          style="left:${ entry.x }%; top:${ entry.y }%"/>
+      `)
 
-      var points = document.getElementsByClassName('point');
+      var points = $('.point');
 
       // thisId will hold a list of all the pointer elements that match 
-      // the id of the current entry.
+      // the id of the current entry, which is linked to a given character.
       var thisId = [];
 
       for (var i = 0; i < points.length; i++) {
@@ -156,42 +134,39 @@ class App {
         var id = points[i].getAttribute('class').match(/\d/)[0];
         var num = parseInt(id);
         // If a particular pointer is not relevant at this step, it will not be 
-        // in the entry's present[] array. Change its opacity to zero.
+        // in the entry variable's present[] array. If so, change its opacity to zero.
         if (entry.present.indexOf(num) === -1) {
           points[i].style.opacity = 0;
         }
-        // Create a list of all the pins that correspond to this entry's id.
+        // Create a list of all the pins that correspond to this character.
         if (entry.id === num) {
           thisId.push(points[i])
         }
       }
 
-      // Loop through the pins corresponding to this entry's id. Set the opacity
-      // of the newest pin to 1. Set the opacity of the previous pin to .3, so that
-      // it is still faintly visible. Set the opacity of earlier pins to 0.
+      // Loop through this character's pins. Set the opacity
+      // of the previous pin to .3. Hide earlier pins
+      // by setting their opacity to 0.
       if (thisId.length > 1) {
-        thisId.forEach((elem, i) => {
+        for (var i = 0; i < thisId.length; i++) {
           if (i === thisId.length - 2) {
             thisId[i].style.opacity = .3;
           }
           else {
             thisId[i].style.opacity = 0;
           }
-        })
-      }
+        };
+      };
+
       thisId[thisId.length-1].style.opacity = 1;
-    }
+    };
 
-    else {
-      return
-    }
-
-    // Display the started or finished message as needed.
+    // Display the finished message, or update the progress bar 
+    // to show how much the user has progressed.
     this.getMessage();
+    this.progressBar.fill(step);
+  };
 
-    // Update the progress bar to show how much the user has progressed.
-    this.progressBar.fill(this.step);
-  }
 
   /* 
    * Check whether started is set to false, or if finished is set to true.
@@ -212,7 +187,10 @@ class App {
     else if (this.finished) {
       var msg = `<div 
         id="finished">
-          <a class="finished-link" href="http://www.ajc.com/news/news/crime-law/atlanta-police-shooting-goes-to-grand-jury-murder-/nsPgj/">Read more</a>
+          <a class="finished-link" 
+            href="http://www.ajc.com/news/news/crime-law/atlanta-police-shooting-goes-to-grand-jury-murder-/nsPgj/">
+            Read more
+          </a>
         </div>`
       $('#message').append(msg);
     }
