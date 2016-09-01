6d69f41a49cd87ccc23aa9d688e7d1e2f4c37915
Cox <jcox@NATL2236LJCOX.local>
2016-09-01 14:27:05 -0400

    added gulp task to build for production

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+  argv = require('yargs').argv,
   rename = require('gulp-rename'),
   buffer = require('vinyl-buffer'), // Vinyl is an interface between browserify and gulp
   livereload = require('gulp-livereload'),
@@ -9,12 +10,17 @@ var gulp = require('gulp'),
   browserify = require('browserify'),
   bs = require('browser-sync'),
   babelify = require('babelify'),
-  webserver = require('gulp-webserver');
-  merge = require('merge-stream');
+  webserver = require('gulp-webserver'),
+  merge = require('merge-stream'),
+  uglify = require('gulp-uglify'),
+  runSequence = require('run-sequence');
 
 var SRC = './assets/';
 var PROD = './dist/prod/';
 var DEV = './dist/dev/';
+
+var ENV = argv.production ? PROD : DEV;
+console.log(argv, argv.production)
 
 var browserSync = {
   port: 8080,
@@ -23,7 +29,15 @@ var browserSync = {
   }
 }
 
-gulp.task('compile', function() {
+gulp.task('compile', ['bundle'], function() {
+  if (ENV === PROD) {
+    return gulp.src(ENV + 'main.js')
+      .pipe(uglify())
+      .pipe(gulp.dest(ENV))
+  }
+});
+
+gulp.task('bundle', function() {
   var b = browserify({
     entries: SRC + 'js/index.js',
     debug: true,
@@ -32,31 +46,30 @@ gulp.task('compile', function() {
     })]
   });
 
-  return b.bundle()
+  return bundle = b.bundle()
     .pipe(source(SRC + 'js/index.js'))
     .pipe(buffer())
     .pipe(rename('main.js'))
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(DEV));
-});
+    .pipe(gulp.dest(ENV));
+})
 
 gulp.task('sass', function(){
   return gulp.src([SRC + 'css/*.scss', SRC + 'css/**/*.css'])
     .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest(DEV + 'css/'));
+    .pipe(gulp.dest(ENV + 'css/'));
 });
-
 
 gulp.task('build-assets', function(done) {
   var html = gulp.src(SRC + 'index.html')
-  .pipe(gulp.dest(DEV));
+  .pipe(gulp.dest(ENV));
 
   var fonts = gulp.src(SRC + 'css/fonts/**/*.otf')
-    .pipe(gulp.dest(DEV + 'css/fonts'));
+    .pipe(gulp.dest(ENV + 'css/fonts'));
 
   var images = gulp.src(SRC + 'img/**')
-    .pipe(gulp.dest(DEV + 'img'));
+    .pipe(gulp.dest(ENV + 'img'));
 
   return merge(html, fonts, images); // Merge emits events from multiple streams
 })
@@ -73,9 +86,13 @@ gulp.task('watch-assets', function() {
   return gulp.watch([SRC + 'index.html', SRC + 'img/*'], ['build-assets']);
 })
 
-gulp.task('serve-dev', ['sass', 'compile', 'build-assets', 
+gulp.task('serve', ['sass', 'compile', 'build-assets', 
 'watch-sass', 'watch-js', 'watch-assets'], function() {
   bsIns = bs.create();
   bsIns.init(browserSync);
   bsIns.reload();
+  console.log('Serving from ' + ENV)
 });
+
+gulp.task('build', ['sass', 'compile', 'build-assets'], function() {
+})
