b0dd0314c1674f11a8b35f49ce76d22614e2a134
Cox <jcox@NATL2236LJCOX.local>
2016-09-09 17:50:42 -0400

    added dummy data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 import * as L from 'leaflet';
 import $ from 'jquery';
 
-var map = L.map('map').setView([30.267, -97.743], 12);
+var map = L.map('map').setView([33.7, -84.3], 12);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   maxZoom: 18,
@@ -9,20 +9,36 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: 'pk.eyJ1IjoiZ2Vlemhhd2siLCJhIjoiY2ltcDFpY2dwMDBub3VtbTFkbWY5b3BhMSJ9.4mN7LI5CJMCDFvqkx1OJZw'
 }).addTo(map);
 
-var precinctBoundary = new L.geoJson();
-precinctBoundary.addTo(map);
+// Get shapefiles.
+function getPrecincts(cb) {
+  $.ajax({
+    dataType: 'json',
+    url: './atlanta-precincts.json',
+    success: function(data) {
+      cb(data)
+    },
+    failure: function() {
+      console.log('failed to get precincts.');
+    }
+  });
+};
 
-// Get shapefiles
-$.ajax({
-  dataType: 'json',
-  url: './atlanta-precincts.json',
-  success: function(data) {
-    $(data).each(function(key, data) {
-      precinctBoundary.addData(data);
-      console.log(data);
-    });
-  },
-  failure: function() {
-    console.log('get failed');
-  }
-});
+var features = [];
+
+// Set the color of the precinct polygon and append to the 
+// map.
+function drawPrecincts(precincts) {
+  $(precincts).each(function(key, feature) {
+    features.push(feature);
+  });
+
+  L.geoJson(features, {style: function(feature) {
+      switch (feature.properties.party) {
+        case 'Republican': return {color: 'red'};
+        case 'Democrat': return {color: 'blue'};
+      };
+    }
+  }).addTo(map);
+};
+
+getPrecincts(drawPrecincts)
--- filter.py
+++ filter.py
@@ -1,21 +1,25 @@
 import json
 from collections import Counter
 
-data = json.loads(open('dist/dev/precincts.geojson', 'r').read())
+data = json.loads(open('assets/data/all_precincts.json', 'r').read())
 features = data['features']
 
 filtered = [] # The array that will hold all precincts from metro Atlanta counties
 cnt = Counter() # Use a counter to check that the number of precincts per county is correct
 
 counties = ['121', '089', '135', '067', '063', '077', '097', '113', '151']
-for precinct in features:
+for i, precinct in enumerate(features):
     county_name = precinct['properties']['COUNTYFP10'] 
     if county_name in counties:
+        if i % 2 == 0:
+            precinct['properties']['party'] = 'Republican'
+        else: 
+            precinct['properties']['party'] = 'Democrat'
         filtered.append(precinct)
         cnt[county_name] += 1
 
 print cnt
 
-with open('atlanta-precincts.json', 'w') as f:
+with open('assets/data/atlanta-precincts.json', 'w') as f:
     f.write(json.dumps(filtered))
 
